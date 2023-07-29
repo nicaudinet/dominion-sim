bbb5c22feb01b706238143c0e2d934243fd6047d
Nicolas Audinet <nic.audinet27@gmail.com>
2023-07-29 23:08:31 +0200

    Debug player option

--- dominion.py
+++ dominion.py
@@ -95,29 +95,37 @@ class Board:
         return self.piles["province"] == 0 or self.num_empty_piles() == 3
 
 class Player():
-    def __init__(self, board):
+    def __init__(self, board, debug=False):
         self.board = board
-        self.discard_pile = []
-        self.hand = []
+        self.debug = debug
 
         coppers = [Treasure("copper") for _ in range(7)]
         estates = [Victory("estate") for _ in range(3)]
         self.deck = coppers + estates
         random.shuffle(self.deck)
+        self.hand = []
+        self.discard_pile = []
 
         self.draw_hand()
+
+    def all_cards(self):
+        return self.deck + self.hand + self.discard_pile
 
     def recycle(self):
         random.shuffle(self.discard_pile)
         self.deck += self.discard_pile
         self.discard_pile = []
 
-    def draw_hand(self):
-        if len(self.deck) < 5:
+    def draw(self, n):
+        if len(self.deck) < n:
             self.recycle()
-        self.hand = self.deck[:5]
-        self.deck = self.deck[5:]
-        # print([c.name for c in self.hand])
+        self.hand += self.deck[:n]
+        self.deck = self.deck[n:]
+
+    def draw_hand(self):
+        self.hand = []
+        self.draw(5)
+        if self.debug: print(sorted([c.name for c in self.hand]))
 
     def discard_hand(self):
         self.discard_pile += self.hand
@@ -128,8 +136,7 @@ class Player():
         return sum(c.value for c in treasure_cards)
 
     def total_points(self):
-        all_cards = self.deck + self.hand + self.discard_pile
-        victory_cards = filter(lambda c: c.is_victory(), all_cards)
+        victory_cards = filter(lambda c: c.is_victory(), self.all_cards())
         return sum(c.points for c in victory_cards)
 
     def buy(self, card_name):
@@ -139,7 +146,7 @@ class Player():
                 return False
             else:
                 self.discard_pile += [card]
-                # print(f"Bought {card_name}")
+                if self.debug: print(f"Bought {card_name}")
                 return True
 
 def big_money(player):
@@ -165,7 +172,6 @@ def play_game():
     while True:
         if play_turn(player1): break
         if play_turn(player2): break
-        # print("--")
     
     return player1.total_points(), player2.total_points()
 
