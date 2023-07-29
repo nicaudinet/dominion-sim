1acf7261f1342edb5284155db6315155d03aefc9
Nicolas Audinet <nic.audinet27@gmail.com>
2023-07-29 23:10:51 +0200

    SmithyBigMoney strategy

--- dominion.py
+++ dominion.py
@@ -16,6 +16,14 @@ class Card:
     def is_action(self):
         return hasattr(self, "actions")
 
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, Card):
+            return self.name == other.name
+        else:
+            raise Exception("Cannot compare Card to something that is not a string or a Card")
+
 class Treasure(Card):
     def __init__(self, card_name):
         super().__init__(card_name)
@@ -46,7 +54,7 @@ class Board:
             # "cellar": 12,
             # "moat": 12,
             # "village": 12,
-            # "smithy": 12,
+            "smithy": 12,
             # "remodel": 12,
             # "workshop": 12,
             # "mine": 12,
@@ -172,11 +180,48 @@ class BigMoney:
         elif treasure_total >= 3:
             self.player.buy("silver")
 
+class SmithyBigMoney:
+    """
+    A classic variation on Big Money, where the player buys a single Smithy as
+    soon as possible and then plays standard Big Money for the rest of the game
+    """
+    def __init__(self, player):
+        self.player = player
+        self.has_smithy = False
+
+    def _action_phase(self):
+        if "smithy" in self.player.hand:
+            self.player.draw(3)
+
+    def _buy_phase(self):
+        treasure_total = self.player.hand_value()
+        if not self.has_smithy:
+            if treasure_total >= 8:
+                self.player.buy("province")
+            elif treasure_total >= 6:
+                self.player.buy("gold")
+            elif treasure_total >= 4:
+                self.player.buy("smithy")
+                self.has_smithy = True
+            elif treasure_total >= 3:
+                self.player.buy("silver")
+        else:
+            if treasure_total >= 8:
+                self.player.buy("province")
+            elif treasure_total >= 6:
+                self.player.buy("gold")
+            elif treasure_total >= 3:
+                self.player.buy("silver")
+
+    def play(self):
+        self._action_phase()
+        self._buy_phase()
+
 def play_game():
 
     board = Board()
     player1 = Player(board, BigMoney)
-    player2 = Player(board, BigMoney)
+    player2 = Player(board, SmithyBigMoney)
 
     while True:
         if player1.play(): break
