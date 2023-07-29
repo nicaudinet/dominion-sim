0dadf951a166e6645a1a599d3d053392f8cec566
Nicolas Audinet <nic.audinet27@gmail.com>
2023-07-29 23:26:27 +0200

    DoubleSmithy strategy

--- dominion.py
+++ dominion.py
@@ -217,10 +217,47 @@ class SmithyBigMoney:
         self._action_phase()
         self._buy_phase()
 
+class DoubleSmithy:
+    """
+    Another variation on Big Money, where the player buys two Smithy cards as
+    soon as possible and then plays standard Big Money for the rest of the game
+    """
+    def __init__(self, player):
+        self.player = player
+        self.n_smithy = 0
+
+    def _action_phase(self):
+        if "smithy" in self.player.hand:
+            self.player.draw(3)
+
+    def _buy_phase(self):
+        treasure_total = self.player.hand_value()
+        if self.n_smithy < 2:
+            if treasure_total >= 8:
+                self.player.buy("province")
+            elif treasure_total >= 6:
+                self.player.buy("gold")
+            elif treasure_total >= 4:
+                self.player.buy("smithy")
+                self.has_smithy = True
+            elif treasure_total >= 3:
+                self.player.buy("silver")
+        else:
+            if treasure_total >= 8:
+                self.player.buy("province")
+            elif treasure_total >= 6:
+                self.player.buy("gold")
+            elif treasure_total >= 3:
+                self.player.buy("silver")
+
+    def play(self):
+        self._action_phase()
+        self._buy_phase()
+
 def play_game():
 
     board = Board()
-    player1 = Player(board, BigMoney)
+    player1 = Player(board, DoubleSmithy)
     player2 = Player(board, SmithyBigMoney)
 
     while True:
